be1a2a3c3d8b26bd5cb0e94a589cf03890d05ca3
Durjoy <da-durjoy@outlook.com>
2021-12-16 19:15:49 +0600

    Constructor pattern with prototype

--- oopJS/ConstructorPattern_2.js
+++ oopJS/ConstructorPattern_2.js
@@ -17,3 +17,23 @@ var array=[uni1,uni2,uni3];
 array.forEach(element => {
     console.log(element.toString());
 });
+
+//----------------------------------------
+//Constructor With Prototype
+//A Function in JavaScript have a property called prototype
+function School(name,year,location) {
+    this.name=name;
+    this.year=year;
+    this.location=location;
+
+    // Note here that we are using Object.prototype.newMethod rather than 
+    // Object.prototype so as to avoid redefining the prototype object
+    School.prototype.toString=function(){return `${this.name} is eastrablished in ${this.year} situated in ${this.location}`};
+}
+var sc1=new School('Govt BB boyes High School',1985,'Tangail');
+var sc2=new School('Mohera Ananda High School',1925,'Mirzapur');
+var sc3=new School('Savar Cantonment School',1975,'Savar');
+var array=[sc1,sc2,sc3];
+array.forEach(element => {
+    console.log(element.toString());
+});
